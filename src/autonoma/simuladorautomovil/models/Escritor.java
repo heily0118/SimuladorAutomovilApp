0eacb99ed5573cc8002220d375a2dc8c933917c2
Heily Rios <heilyayala005@gmail.com>
2025-04-05 17:17:26 -0500

    Se hicieron cambios en el main y se implemento metodo en escritor

--- src/autonoma/simuladorautomovil/models/Escritor.java
+++ src/autonoma/simuladorautomovil/models/Escritor.java
@@ -4,6 +4,9 @@
  */
 package autonoma.simuladorautomovil.models;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  *
  * @author Heily Yohana Rios Ayala <heilyy.riosa@autonoma.edu.co>
@@ -12,5 +15,14 @@ package autonoma.simuladorautomovil.models;
  * @version 1.0.0
  */
 public interface Escritor {
+    
+    /**
+     * Escribe el archivo de memoria interna por lineas
+     * 
+     * @param archivo
+     * @throws java.io.IOException si el archivo no existe
+     */
+    
+    public abstract void escribir(ArrayList<String> archivo) throws IOException ;
     
 }
