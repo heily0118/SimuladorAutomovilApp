ee7a7c65a439440eda9ff9bc36ae17bba71257ad
Heily Rios <heilyayala005@gmail.com>
2025-04-05 15:00:43 -0500

    Se implementon los atributos y metodos de la clase llanta y moto

--- src/autonoma/simuladorautomovil/models/Llanta.java
+++ src/autonoma/simuladorautomovil/models/Llanta.java
@@ -6,8 +6,14 @@ package autonoma.simuladorautomovil.models;
 
 /**
  *
- * @author USUARIO
+ *  
+ * @author Heily Yohana Rios Ayala <heilyy.riosa@autonoma.edu.co>
+ * @since 20250405
+ * @see 
+ * @version 1.0.0
  */
-public class Llanta {
+ 
+public abstract class Llanta {
+    
     
 }
--- src/autonoma/simuladorautomovil/models/Motor.java
+++ src/autonoma/simuladorautomovil/models/Motor.java
@@ -6,8 +6,38 @@ package autonoma.simuladorautomovil.models;
 
 /**
  *
- * @author USUARIO
+ *  
+ * @author Heily Yohana Rios Ayala <heilyy.riosa@autonoma.edu.co>
+ * @since 20250405
+ * @see 
+ * @version 1.0.0
  */
+
 public class Motor {
+    private String tipoMotor;
+    private int velocidadMaxima;
+
+    public Motor(String tipoMotor, int velocidadMaxima) {
+        this.tipoMotor = tipoMotor;
+        this.velocidadMaxima = velocidadMaxima;
+    }
+
+    public String getTipoMotor() {
+        return tipoMotor;
+    }
+
+    public void setTipoMotor(String tipoMotor) {
+        this.tipoMotor = tipoMotor;
+    }
+
+    public int getVelocidadMaxima() {
+        return velocidadMaxima;
+    }
+
+    public void setVelocidadMaxima(int velocidadMaxima) {
+        this.velocidadMaxima = velocidadMaxima;
+    }
+    
+    
     
 }
