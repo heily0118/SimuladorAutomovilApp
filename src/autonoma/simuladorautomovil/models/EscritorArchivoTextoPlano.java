f26e049c0028ff952f7555c115f887a173af0d4b
Heily Rios <heilyayala005@gmail.com>
2025-04-05 16:46:34 -0500

    Se crearon las clases lector y escritor de archivo plano

---
+++ src/autonoma/simuladorautomovil/models/EscritorArchivoTextoPlano.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.simuladorautomovil.models;
+
+/**
+ *
+ * @author Heily Yohana Rios Ayala <heilyy.riosa@autonoma.edu.co>
+ * @since 20250405
+ * @see 
+ * @version 1.0.0
+ */
+public class EscritorArchivoTextoPlano implements Escritor {
+    
+}
