693dd8d52776288875ba33afaeadd26b94b5d8a6
María Paz Puerta Acevedo <mariap.puertaa@autonoma.edu.co>
2025-04-06 15:43:54 -0500

    Se agregó la documentación del código de la clase Llanta

--- src/autonoma/simuladorautomovil/models/Llanta.java
+++ src/autonoma/simuladorautomovil/models/Llanta.java
@@ -14,31 +14,61 @@ package autonoma.simuladorautomovil.models;
  */
  
 public abstract class Llanta {
+    /**
+     * Velocidad máxima a la que se desgasta la llanta.
+     */
     private int velocidadMaxima;
+    /**
+     * Nivel de desgaste de la llanta.
+     */
     private int desgaste; // 0 a 100
 
+    /**
+     * Constructor de la clase Llanta.
+     * @param velocidadMaxima Es la velocidad máxima a la que se desgasta la llanta.
+     */
     public Llanta(int velocidadMaxima) {
         this.velocidadMaxima = velocidadMaxima;
         this.desgaste = 0;
     }
 
+    /**
+     * Obtiene la velocidad máxima recomendada para la llanta.
+     * @return Retorna la velocidad máxima en km/h.
+     */
     public int getVelocidadMaxima() {
         return velocidadMaxima;
     }
 
+    /**
+     * Establece una nueva velocidad máxima recomendada para la llanta.
+     * @param velocidadMaxima Retorna la velocidadMaxima Nueva velocidad máxima en km/h.
+     */
     public void setVelocidadMaxima(int velocidadMaxima) {
         this.velocidadMaxima = velocidadMaxima;
     }
 
+    /**
+     * Obtiene el nivel actual de desgaste de la llanta.
+     * @return Retorna el desgaste actual.
+     */
     public int getDesgaste() {
         return desgaste;
     }
 
+    /**
+     * Establece un nuevo nivel de desgaste para la llanta.
+     * @param desgaste Retorna el valor de desgaste a establecer.
+     */
     public void setDesgaste(int desgaste) {
         this.desgaste = desgaste;
     }
 
-   
+    /**
+     * Aumenta el desgaste de la llanta en un valor específico, lo que 
+     * quiere decir que si el valor total supera 100, se ajusta a 100.
+     * @param valor Es el valor Cantidad de desgaste a añadir.
+     */
     public void aumentarDesgaste(int valor) {
         this.desgaste += valor;
         if (this.desgaste > 100) {
@@ -46,9 +76,16 @@ public abstract class Llanta {
         }
     }
 
-    
+
+    /**
+     * Método abstracto que define cómo se incrementa el desgaste de la llanta según la velocidad de uso.
+     * @param velocidad Es la Velocidad actual del vehículo.
+     */
     public abstract void aumentarDesgastePorUso(int velocidad);
 
-    
+    /**
+     * Método abstracto para verificar el estado del desgaste de la llanta.
+     * @return Retorna el mensaje con el estado del desgaste de la llanta.
+     */
     public abstract String verificarDesgaste();
 }
