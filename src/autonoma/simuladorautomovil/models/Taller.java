098a2f631b1912a162d03418f9367e84fa078528
Heily Rios <heilyayala005@gmail.com>
2025-04-10 23:21:46 -0500

    se agregar atributos y metodos a la clase taller

--- src/autonoma/simuladorautomovil/models/Taller.java
+++ src/autonoma/simuladorautomovil/models/Taller.java
@@ -6,8 +6,72 @@ package autonoma.simuladorautomovil.models;
 
 /**
  *
- * @author USUARIO
+ *@author Heily Yohana Rios Ayala <heilyy.riosa@autonoma.edu.co>
+ * @since 20250405
+ * @see autonoma.simuladorautomovil.models.Vehiculo
+ * @version 1.0.0
  */
 public class Taller {
+    
+  
+   private Vehiculo vehiculo;
+
+    public Taller(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo;
+    }
+
+    public Vehiculo getVehiculo() {
+        return vehiculo;
+    }
+
+    public void setVehiculo(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo;
+    }
+    
+    
+    
+    public String configurarLlantas(String tipo){
+        Llanta llanta;
+
+        switch (tipo.toLowerCase()) {
+            case "buena":
+                llanta = new Llanta(110, "Buena");
+                break;
+            case "bonita":
+                llanta = new Llanta(70, "Bonita");
+                break;
+            case "barata":
+                llanta = new Llanta(50, "Barata");
+                break;
+            default:
+                return "Tipo de llanta inválido: " + tipo;
+        }
+
+        this.vehiculo.setLlantas(llanta);
+        return "Llanta configurada: " + llanta.getTipoLlanta();
+    }
+    
+    public String configurarMotor(String cilindraje){
+        Motor motor;
+
+        switch (cilindraje) {
+            case "1000":
+                motor = new Motor("1000", 100);
+                break;
+            case "2000":
+                motor = new Motor("2000", 160);
+                break;
+            case "3000":
+                motor = new Motor("3000", 220);
+                break;
+            default:
+                return "Cilindraje de motor inválido: " + cilindraje;
+        }
+
+        this.vehiculo.setMotor(motor);
+        return "Motor configurado: " + motor.getCilindraje();
+    
+    }
+    
     
 }
