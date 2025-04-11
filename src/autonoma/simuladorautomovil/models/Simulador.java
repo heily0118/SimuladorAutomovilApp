056fef6f342531e3c5001c443baee1cebf00047e
María Paz Puerta Acevedo <mariap.puertaa@autonoma.edu.co>
2025-04-10 23:49:09 -0500

    Se creó la clase Simulador

---
+++ src/autonoma/simuladorautomovil/models/Simulador.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.simuladorautomovil.models;
+
+/**
+ *
+ * @author María Paz Puerta Acevedo <mariap.puertaa@autonoma.edu.co>
+ * @since 20250405
+ * @see autonoma.simuladorautomovil.models.Vehiculo
+ * @version 1.0.0
+ */
+public class Simulador {
+    
+}
